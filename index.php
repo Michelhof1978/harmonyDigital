2685ce80ef0e21bf3c7290567bb97431943beb88
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2024-06-26 16:11:12 +0200

    header

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include("head.php") ?>
+    <meta name="description" content="">
+    <title>Harmony Digital</title>
+</head>
+<body>
+    <?php include("header.php") ?>
+
+    <div id="content">
+        <!-- Autres sections de contenu ici -->
+        
+        <div id="star-container">
+       
+        </div>
+        
+        <!-- Autres sections de contenu ici -->
+    </div>
+
+    <?php include("footer.php") ?>
