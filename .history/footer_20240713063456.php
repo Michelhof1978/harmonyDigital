e254677e32d11560103d651d16715605e6051580
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2024-07-13 06:36:20 +0200

    suppression fichier script vers footer

--- .history/footer_20240713063456.php
+++ .history/footer_20240713063456.php
@@ -50,41 +50,6 @@
 
 <!-- _____________________________________________________________________________________ -->
 
-<!-- Arrière plan 0 et 1 -->
-<script>
-  const chars = '01';
-
-function createStarLines() {
-  const lineCount = 30; // Nombre de lignes de lettres et chiffres
-  const charsPerLine = 110; // Nombre de caractères par ligne
-
-  const starContainer = document.getElementById('star-container');
-  if (!starContainer) return; // S'assurer que l'élément existe
-
-  for (let i = 0; i < lineCount; i++) {
-    const line = document.createElement('div');
-    line.className = 'star-line';
-
-    for (let j = 0; j < charsPerLine; j++) {
-      const char = chars.charAt(Math.floor(Math.random() * chars.length));
-      const span = document.createElement('span');
-      span.textContent = char;
-      span.className = 'star';
-      span.style.animationDelay = `${Math.random() * 1550}s`; // Délai d'animation aléatoire
-      line.appendChild(span);
-    }
-
-    starContainer.appendChild(line); // Ajouter la ligne au conteneur
-  }
-}
-
-// Générer les lignes de lettres et chiffres scintillants au chargement de la page
-document.addEventListener('DOMContentLoaded', function() {
-  createStarLines();
-});
-
-</script>
-
 
 <!-- _____________________________________________________________________________________ -->
 
@@ -165,6 +130,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
 <!--____________________________________________________________________________________-->
 
+<!-- Arrière plan 0 et 1 -->
+<script>
+  const chars = '01';
+
+function createStarLines() {
+  const lineCount = 30; // Nombre de lignes de lettres et chiffres
+  const charsPerLine = 110; // Nombre de caractères par ligne
+
+  const starContainer = document.getElementById('star-container');
+  if (!starContainer) return; // S'assurer que l'élément existe
+
+  for (let i = 0; i < lineCount; i++) {
+    const line = document.createElement('div');
+    line.className = 'star-line';
+
+    for (let j = 0; j < charsPerLine; j++) {
+      const char = chars.charAt(Math.floor(Math.random() * chars.length));
+      const span = document.createElement('span');
+      span.textContent = char;
+      span.className = 'star';
+      span.style.animationDelay = `${Math.random() * 1550}s`; // Délai d'animation aléatoire
+      line.appendChild(span);
+    }
+
+    starContainer.appendChild(line); // Ajouter la ligne au conteneur
+  }
+}
+
+// Générer les lignes de lettres et chiffres scintillants au chargement de la page
+document.addEventListener('DOMContentLoaded', function() {
+  createStarLines();
+});
+
+</script>
 
 </body>
 </html>
