9d8d3a8d3f64c6661f20f4d67468197179347f8c
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2024-07-26 19:02:14 +0200

    footer

--- .history/footer_20240726092949.php
+++ .history/footer_20240726092949.php
@@ -4,19 +4,19 @@
 
 <div class="img3 col-12 pt-3">
     <img class="img2" src="../images/logo.webp" loading="lazy" alt="logo">
-    <p class="copyright d-flex flex-wrap text-sm justify-content-center">Site réalisé par &nbsp; <a href="" target="_blank"><strong>Michel Hoffmann - Harmony Digital</strong></a></p>
+    <p class="copyright d-flex flex-wrap text-sm justify-content-center">Site réalisé par &nbsp; <a href="<?php echo AUTHOR_URL; ?>" target="_blank"><strong>Michel Hoffmann - Harmony Digital</strong></a></p>
 
 
     <div class="mb-2">
         <!-- Facebook -->
-        <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998;" href="" target="_blank" role="button">
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998;" href=<?php echo FACEBOOK_URL; ?> target="_blank" role="button">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                 <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
             </svg>
         </a>
 
         <!-- office du tourisme -->
-        <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee;" href="" target="_blank" role="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee;" href="<?php echo TOURISM_URL; ?>" target="_blank" role="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                 <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
             </svg></a>
