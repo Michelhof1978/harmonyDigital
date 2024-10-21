15fbaaa387f365228d566d1acb582040ca3e0027
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2024-10-21 11:34:30 +0200

    optimisation js temps téléchargement + tarifs

--- .history/tarifs_20241021113053.php
+++ .history/tarifs_20241021113053.php
@@ -265,10 +265,10 @@
 </section>
 
 <div class="mt-5 mb-5 d-flex justify-content-center align-items-center" style="min-height: 50vh;">
-<video autoplay loop muted playsinline class="rounded video-responsive lazy" preload="none" data-src="videos/iatarif.mp4">
-    <source type="video/mp4">
-    Votre navigateur ne supporte pas la balise <code>video</code>.
-</video>
+    <video autoplay loop muted playsinline class="rounded video-responsive">
+        <source src="videos/iatarif.mp4" type="video/mp4">
+        Votre navigateur ne supporte pas la balise <code>video</code>.
+    </video>
 </div>
 
 
@@ -357,10 +357,10 @@
 <div class="container-fluid px-0 mb-5 d-none d-md-block">
     <div class="d-flex justify-content-center align-items-center" style="min-height: 50vh; margin-top:-50px; margin-bottom:-100px;">
         <div class="video-container" style="max-width: 50%; margin: auto;">
-        <video autoplay loop muted playsinline class="rounded w-100 lazy" preload="none" data-src="videos/robotpresentation1.mp4">
-    <source type="video/mp4">
-    Votre navigateur ne supporte pas la balise <code>video</code>.
-</video>
+            <video autoplay loop muted playsinline class="rounded w-100">
+                <source src="videos/robotpresentation1.mp4" type="video/mp4">
+                Votre navigateur ne supporte pas la balise <code>video</code>.
+            </video>
         </div>
     </div>
 </div>
@@ -370,10 +370,10 @@
 <div class="container-fluid px-0 mb-5 d-block d-md-none">
     <div class="d-flex justify-content-center align-items-center" style="min-height: 50vh; margin-top:-50px; margin-bottom:-100px;">
         <div class="video-container" style="width: 100%;">
-        <video autoplay loop muted playsinline class="rounded w-100 lazy" preload="none" data-src="videos/robotpresentation1.mp4">
-    <source type="video/mp4">
-    Votre navigateur ne supporte pas la balise <code>video</code>.
-</video>
+            <video autoplay loop muted playsinline class="rounded w-100">
+                <source src="videos/robotpresentation1.mp4" type="video/mp4">
+                Votre navigateur ne supporte pas la balise <code>video</code>.
+            </video>
         </div>
     </div>
 </div>
